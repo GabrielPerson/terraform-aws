a87501e6a85a55d0e4834e1a0fbda4490996cb73
GabrielPerson <gabrielperson.pm@gmail.com>
2026-05-02 16:56:54 -0300

    delete old buckets

--- s3.tf
+++ s3.tf
@@ -5,36 +5,6 @@ resource "random_id" "suffix" {
     byte_length = 4
 }
 
-resource "aws_s3_bucket" "labpessoal01-bucket-001" {
-    bucket = "labpessoal01-bucket-gabrielmelo-${random_id.suffix.hex}"
-
-    tags = {
-        Name        = "labpessoal01-bucket-001"
-        project     = "lab01"
-        ManagedBy   = "terraform"
-    }
-}
-
-resource "aws_s3_object" "voidjpg" {
-    bucket = aws_s3_bucket.labpessoal01-bucket-001.id
-    key    = "voidjpg"
-    source = "s3files/TheVoid.jpg"
-    
-    tags = {
-        Name      = "voidjpg"
-        ManagedBy = "terraform"
-    }
-}
-
-resource "aws_s3_bucket_public_access_block" "labpessoal01-bucket-001-pab" {
-    bucket = aws_s3_bucket.labpessoal01-bucket-001.id
-
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-}
-
 #resource "aws_s3_bucket_policy" "deny_unencrypted_uploads" {
 #    bucket = aws_s3_bucket.this.id
 #
