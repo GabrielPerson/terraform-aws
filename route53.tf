d1fa28b022ddca7335f78accf40ae9d0a88f3304
GabrielPerson <gabrielperson.pm@gmail.com>
2026-05-07 20:35:38 -0300

    update record zoneid

--- route53.tf
+++ route53.tf
@@ -55,7 +55,7 @@ resource "aws_route53_record" "gabrielpersonclick_acm_validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.main.zone_id
+  zone_id         = aws_route53_zone.gabrielpersonclick.zone_id
 }
 
 # Load Balancer Alias Record - A record pointing to ALB
